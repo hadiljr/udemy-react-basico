66cb50c938cd3fea8c1afaad1c8cc62948608d5d
Hadil Aparecido Dos Santos Junior <hadil.junior@ambevtech.com.br>
2022-07-12 18:13:13 -0300

    Aula Criando Componentes

---
+++ src/components/Comentario.js
@@ -0,0 +1,12 @@
+import './Comentario.css'
+import React from "react";
+
+//JSX
+const Comentario = () => (
+    <div className='Comentario'>
+        <div>José</div>
+        <div>Comentario</div>
+    </div>
+);
+
+export default Comentario;
