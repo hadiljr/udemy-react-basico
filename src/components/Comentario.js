80b280ef54c36124a69325248f2e507c3c446c2c
Hadil Aparecido Dos Santos Junior <hadil.junior@ambevtech.com.br>
2022-07-18 20:04:44 -0300

    Aula Instalando Pacotes

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,5 +1,7 @@
-import './Comentario.css'
 import React from "react";
+import { formatRelative } from "date-fns";
+import {ptBR} from 'date-fns/locale'
+import './Comentario.css'
 import avatarImg from './user.png'
 
 //JSX
@@ -15,7 +17,7 @@ const Comentario = props => {
             <h3 className='nome'>{props.nome}</h3>        
             <p className='email'>{props.email}</p>
             <p className='mensagem'>{props.children}</p>
-            <p className='data'>{props.data.toString()}</p>    
+            <p className='data'>{formatRelative(props.data, new Date(),{locale:ptBR})}</p>    
             <button onClick={props.onRemove}>&times;</button>
         </div>
         
